da45d8c7b3497d723cb158d4f423fffef2a63844
minsky922 <minsky922@gmail.com>
2024-01-09 10:12:36 +0900

    초기 설정 입니다.

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import datetime
+import hashlib
 
+import jwt
 import requests
 from bs4 import BeautifulSoup
+from bson import ObjectId
+from flask import Flask, jsonify, redirect, render_template, request, url_for
+from pymongo import MongoClient
 
+# Flask 애플리케이션 초기화
+app = Flask(__name__)
+
+# MongoDB 클라이언트 설정
+client = MongoClient("localhost", 27017)
+db = client.dbtest
+
+# JWT 토큰 생성을 위한 비밀 키 - 이 키는 비밀로 유지되어야 합니다
+SECRET_KEY = 'JUNGLE'
 
 
 ## URL 별로 함수명이 같거나,
