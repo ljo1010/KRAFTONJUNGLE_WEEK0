8a7f765ad9516330d9770c9a00cf40e9bc9a8ffb
minsky922 <minsky922@gmail.com>
2024-01-09 15:44:42 +0900

    login, register success

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def api_login():
       return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
    
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5001, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
